04ca7d0f563bfee73cfbcee96dff7e86899248fb
SORNAASAI <sornaarajalakshmi@gmail.com>
2024-08-12 16:19:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//import Greeting from './components/Greeting';
+import Sample from './components/sample';
+import Changethestate from './components/Changethestate';
+import Dept from './components/twocom';
+import Counter from './components/Counter';
+import Togglemessage from './components/Togglemessageclass3';
+function App()
+{
+    return (
+        <div>
+            <Sample/>
+            <Changethestate/>
+
+    <Dept/>
+    <Counter/>
+    <Togglemessage/>
+        </div>
+    )
+}
+export default App;
